a621b87083520b3973faaaf5771d1c98082c31a6
Surahman <surahman.duang@gmail.com>
2015-12-25 11:12:03 +0700

    create tag form widget

---
+++ formwidgets/Tagbox.php
@@ -0,0 +1,65 @@
+<?php namespace Rahman\BlogTags\FormWidgets;
+
+use Backend\Classes\FormWidgetBase;
+use Config;
+
+class TagBox extends FormWidgetBase
+{
+    public function widgetDetails()
+    {
+        return [
+            'name'        => 'Tag Box Field',
+            'description' => 'Tagbox using AJAX'
+        ];
+    }
+
+    /**
+     * render the form widget
+     */
+    public function render() {
+        $this->prepareVars();
+        return $this->makePartial('widgets');
+    }
+
+    /**
+     * prepare the variables
+     *
+     * @return void
+     */
+    public function prepareVars()
+    {
+        $this->vars['id'] = $this->model->id;
+    }
+    
+
+    /**
+     * load assets widgets
+     */
+    public function loadAssets()
+    {
+        $this->addCss('css/jquery.taghandler.css');
+        $this->addCss('css/jquery-ui-1.8.2.custom.css');
+
+        $this->addJs('js/jquery-ui-11.min.js');
+        $this->addJs('js/jquery.taghandler.min.js');
+    }
+
+    /**
+     * get tag id if exists or create it if not exists
+     *
+     * @return array
+     */
+    public function getSaveValue($value)
+    {
+        $tags = explode(",", implode(",", $value));
+        $ids = [];
+
+        foreach ($tags as $name) {
+            $created = Tag::firstOrCreate(['name' => $name]);
+
+            $ids[] = $created->id;
+        }
+        return $ids;
+    }
+    
+}
---
+++ routes.php
@@ -0,0 +1,21 @@
+<?php
+use Rahman\BlogTags\Models\Tag;
+use RainLab\Blog\Models\Post;
+
+/**
+ * get available tags and assigned tags by post id
+ */
+Route::get('api/blog/post/{postId?}', function($postId = 0) {
+    $availableTags = Tag::all()->lists('name');
+    $assignedTags  = Tag::whereHas('posts', function($q) use ($postId) { 
+        $q->where('id', $postId);
+    })->lists('name');
+
+    $response = [
+        'assignedTags'  => $assignedTags,
+        'availableTags' => $availableTags
+    ];
+
+    return Response::json($response);
+});
+
