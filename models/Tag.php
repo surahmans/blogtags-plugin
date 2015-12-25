e9f7350e1b29a8b0f52f137e2f58a0b0c6793bb5
Surahman <surahman.duang@gmail.com>
2015-12-25 10:37:26 +0700

    create blog tags and pivot table

---
+++ models/Tag.php
@@ -0,0 +1,39 @@
+<?php namespace Rahman\Blogtags\Models;
+
+use Model;
+
+/**
+ * Tag Model
+ */
+class Tag extends Model
+{
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'rahman_blogtags_tags';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $hasOne = [];
+    public $hasMany = [];
+    public $belongsTo = [];
+    public $belongsToMany = [];
+    public $morphTo = [];
+    public $morphOne = [];
+    public $morphMany = [];
+    public $attachOne = [];
+    public $attachMany = [];
+
+}
---
+++ updates/create_tags_table.php
@@ -0,0 +1,41 @@
+<?php namespace Rahman\Blogtags\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateTagsTable extends Migration
+{
+
+    public function up()
+    {
+        if ( ! Schema::hasTable('rahman_blogtags_tags')){
+            Schema::create('rahman_blogtags_tags', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->string('name', 25)->nullable();
+                $table->string('slug', 27)->nullable()->index();
+                $table->timestamps();
+            });
+        }
+
+        if (! Schema::hasTable('rahman_blogtags_posts_tags')) {
+            Schema::create('rahman_blogtags_posts_tags', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->integer('tag_id')->unsigned();
+                $table->integer('post_id')->unsigned();
+                $table->index(['tag_id', 'post_id']);
+                $table->foreign('tag_id')->references('id')->on('rahman_blogtags_tags')->onDelete('cascade');
+                $table->foreign('post_id')->references('id')->on('rainlab_blog_posts')->onDelete('cascade');
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('rahman_blogtags_tags');
+        Schema::dropIfExists('rahman_blogtags_posts_tags');
+    }
+
+}
